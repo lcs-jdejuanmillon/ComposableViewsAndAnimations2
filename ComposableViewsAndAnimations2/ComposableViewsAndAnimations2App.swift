9bf8f3d98f788970754ac00b4fc3d5c679c4f9aa
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2022-04-29 10:22:07 -0400

    Initial Commit

---
+++ ComposableViewsAndAnimations2/ComposableViewsAndAnimations2App.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableViewsAndAnimations2App.swift
+//  ComposableViewsAndAnimations2
+//
+//  Created by Jacobo de Juan Millon on 2022-04-29.
+//
+
+import SwiftUI
+
+@main
+struct ComposableViewsAndAnimations2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
